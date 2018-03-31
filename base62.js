04969a725ffc7226cfa3e0a3ed2342964f9b24a0
FND <fnd@localhost.localdomain>
2018-03-31 22:01:05 +0200

    Add ES5 distribution
    I'm not convinced this is a great idea:
    * while faucet-pipeline <http://faucet-pipeline.org> makes bundling and
      transpiling easy, it inevitably means we get Rollup and Babel as
      fairly heavyweight dependencies (even though they're only needed for
      development)
    * I very much dislike adding build artifacts (i.e. `base62.js`) to the
      source repository - the alternative would be to only add it to the
      npm package, but that would require a release script and process
    
    it might be better to leave bundling/transpiling to users - if that's
    even necessary anymore, as ES6 support is becoming increasingly
    widespread (plus there's always the v1.x line to fall back on,
    particularly because functionality-wise there's no difference)

---
+++ base62.js
@@ -0,0 +1,111 @@
+'use strict';
+
+if(typeof global === "undefined" && typeof window !== "undefined") {
+	window.global = window;
+}
+
+var CHARSET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+
+
+function encode(int) {
+    if (int === 0) {
+        return CHARSET[0];
+    }
+
+    var res = "";
+    while (int > 0) {
+        res = CHARSET[int % 62] + res;
+        int = Math.floor(int / 62);
+    }
+    return res;
+}
+
+function decode(str) {
+    var res = 0;
+    var length = str.length;
+
+    for (var i = 0; i < length; i++) {
+        var char = str.charCodeAt(i);
+        if (char < 58) {
+            char = char - 48;
+        } else if (char < 91) {
+            char = char - 29;
+        } else {
+            char = char - 87;
+        }
+        res += char * Math.pow(62, length - i - 1);
+    }
+    return res;
+}
+
+function encode$1(int, _ref) {
+    var byCode = _ref.byCode;
+
+    if (int === 0) {
+        return byCode[0];
+    }
+
+    var res = "";
+    while (int > 0) {
+        res = byCode[int % 62] + res;
+        int = Math.floor(int / 62);
+    }
+    return res;
+}
+
+function decode$1(str, _ref2) {
+    var byChar = _ref2.byChar;
+
+    var res = 0;
+    var length = str.length;
+
+    for (var i = 0; i < length; i++) {
+        var char = str[i];
+        res += byChar[char] * Math.pow(62, length - i - 1);
+    }
+    return res;
+}
+
+
+function indexCharset(str) {
+    var byCode = {};
+    var byChar = {};
+    for (var i = 0; i < str.length; i++) {
+        var char = str[i];
+        byCode[i] = char;
+        byChar[char] = i;
+    }
+    return { byCode: byCode, byChar: byChar };
+}
+
+var Base62 = {
+    encode: encode,
+    decode: decode,
+    setCharacterSet: function setCharacterSet(charset) {
+        if (charset.length !== 62) {
+            throw Error("You must supply 62 characters.");
+        }
+
+        var uniq = {};
+        for (var i = 0; i < charset.length; i++) {
+            var char = charset[i];
+            if (uniq[char]) {
+                throw Error("You must use unique characters.");
+            }
+            uniq[char] = true;
+        }
+
+        charset = indexCharset(charset);
+        var encode$$1 = encode$1,
+            decode$$1 = decode$1;
+
+        Base62.encode = function (value) {
+            return encode$$1(value, charset);
+        };
+        Base62.decode = function (value) {
+            return decode$$1(value, charset);
+        };
+    }
+};
+
+module.exports = Base62;
---
+++ faucet.config.js
@@ -0,0 +1,10 @@
+"use strict";
+
+module.exports = {
+    js: [{
+        source: "./src/legacy.js",
+        target: "./base62.js",
+        esnext: true,
+        format: "cjs"
+    }]
+};
