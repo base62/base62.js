a9f4c1aff0cbc1316c050dc0cedc336e3b1048a1
Pi Tian <Pi-314159265@users.noreply.github.com>
2020-08-19 02:12:17 -0400

    Update test_ascii.js

--- test/test_ascii.js
+++ test/test_ascii.js
@@ -12,27 +12,27 @@ describe("Base62 codec (ASCII)", function() {
     it("should encode numbers", function() {
         assertSame(encode(0), "0");
         assertSame(encode(7), "7");
-        assertSame(encode(16), "g");
-        assertSame(encode(61), "Z");
+        assertSame(encode(16), "G");
+        assertSame(encode(61), "z");
         assertSame(encode(65), "13");
-        assertSame(encode(999), "g7");
-        assertSame(encode(9999), "2Bh");
-        assertSame(encode(238327), "ZZZ");
-        assertSame(encode(10000000000001), "2Q3rKTOF");
-        assertSame(encode(10000000000002), "2Q3rKTOG");
+        assertSame(encode(999), "G7");
+        assertSame(encode(9999), "2bH");
+        assertSame(encode(238327), "zzz");
+        assertSame(encode(10000000000001), "2q3Rktof");
+        assertSame(encode(10000000000002), "2q3Rktog");
     });
 
     it("should decode strings", function() {
         assertSame(decode("0"), 0);
         assertSame(decode("7"), 7);
-        assertSame(decode("g"), 16);
-        assertSame(decode("Z"), 61);
+        assertSame(decode("G"), 16);
+        assertSame(decode("z"), 61);
         assertSame(decode("13"), 65);
         assertSame(decode("0013"), 65); // ignore leading zeros
-        assertSame(decode("g7"), 999);
-        assertSame(decode("2Bh"), 9999);
-        assertSame(decode("ZZZ"), 238327);
-        assertSame(decode("2Q3rKTOF"), 10000000000001);
-        assertSame(decode("2Q3rKTOH"), 10000000000003);
+        assertSame(decode("G7"), 999);
+        assertSame(decode("2bH"), 9999);
+        assertSame(decode("zzz"), 238327);
+        assertSame(decode("2q3Rktof"), 10000000000001);
+        assertSame(decode("2q3Rktog"), 10000000000003);
     });
 });
